46a151145b6b6e9933f7dbe2c709e76035913a85
PREM SAI GANESH KOPPARAPU <kpremsaiganesh.143@gmail.com>
2023-09-28 12:45:37 -0700

    added product outputting for customers

---
+++ controllers/products.controller.js
@@ -0,0 +1,15 @@
+const Product = require('../models/product.model');
+
+async function getAllProducts(req,res) {
+    try{
+        const products = await Product.findAll();
+        res.render('customer/products/all-products',{products: products});
+    } catch (error) {
+        next(error);
+    }
+    
+}
+
+module.exports = {
+    getAllProducts: getAllProducts
+};
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
+const productsController = require('../controllers/products.controller');
+
 const router = express.Router();
 
-router.get('/products',function(req,res){
-    res.render('customer/products/all-products');
-});
+router.get('/products', productsController.getAllProducts);
 
 
 module.exports =router;
